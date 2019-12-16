8408924df6932039ca02d231669c20c04664c8e4
Alex Arciniaga <alexarciniaga@gmail.com>
2019-12-16 11:06:54 -0600

    Cleaning and pretty-fying files

--- js/global.js
+++ js/global.js
@@ -1,82 +1,34 @@
 var greenanimate = bodymovin.loadAnimation({
-    container: document.getElementById('greenanim'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: greenPath
+  container: document.getElementById("greenanim"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: greenPath
 });
 var redanimate = bodymovin.loadAnimation({
-    container: document.getElementById('redanim'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: redPath
+  container: document.getElementById("redanim"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: redPath
 });
-$('div#greenanim').hover(function() {
-    $('div#greenanim svg').show();
-});
-
-$('div#greenanim').mouseleave(function() {
-    $('div#greenanim svg').hide();
+$("div#greenanim").hover(function() {
+  $("div#greenanim svg").show();
 });
 
-$('div#redanim').hover(function() {
-    $('div#redanim svg').show();
+$("div#greenanim").mouseleave(function() {
+  $("div#greenanim svg").hide();
 });
 
-$('div#redanim').mouseleave(function() {
-    $('div#redanim svg').hide();
+$("div#redanim").hover(function() {
+  $("div#redanim svg").show();
 });
 
-window.addEventListener('DOMContentLoaded', function() {
-    redanimate.setSpeed('0.2');
-    greenanimate.setSpeed('0.2');
+$("div#redanim").mouseleave(function() {
+  $("div#redanim svg").hide();
 });
-// // Load Green bottle animation
 
-// $( window ).on( "load", function() {
-        
-//     var loadingBar1 = document.getElementById("loadingBar1");
-//     var loadingContent1 = document.getElementById("loadingContent1");
-//     var options = {
-//         distID:  '1658',
-//         solution3DName: 'green-true-mild-sauce',
-//         projectName: 'hot-sauce-bottles',
-//       solution3DID:  '652',
-//       containerID: 'green-sauce',
-//       onLoadingChanged: function(loading) {
-//         loadingBar1.style.width = loading.progress + "%";
-//       }
-//     };
-//     Unlimited3D.init(options, {},function(error, status){
-//         loadingContent1.style.display = "none";
-//         if (error || !status) {
-//             console.log(error);
-//             return;
-//         }
-//     });
-// });
-// // Load Red bottle animation 
-
-// $( window ).on( "load", function() {
-        
-//     var loadingBar = document.getElementById("loadingBar2");
-//     var loadingContent = document.getElementById("loadingContent2");
-//     var options = {
-//         distID:  '1659',
-//         solution3DName: 'red-true-mild-sauce-v2',
-//         projectName: 'hot-sauce-bottles',
-//       solution3DID:  '674',
-//       containerID: 'red-sauce',
-//       onLoadingChanged: function(loading) {
-//         loadingBar.style.width = loading.progress + "%";
-//       }
-//     };
-//     Unlimited3D.init(options, {},function(error, status){
-//         loadingContent.style.display = "none";
-//         if (error || !status) {
-//             console.log(error);
-//             return;
-//         }
-//     });
-// });
+window.addEventListener("DOMContentLoaded", function() {
+  redanimate.setSpeed("0.2");
+  greenanimate.setSpeed("0.2");
+});
--- js/green.js
+++ js/green.js
@@ -1,22 +1,21 @@
-$( window ).on( "load", function() {
-        
-    var loadingBar = document.getElementById("loadingBar");
-    var loadingContent = document.getElementById("loadingContent");
-    var options = {
-        distID:  '1507',
-        solution3DName: 'green-true-mild-sauce',
-        projectName: 'hot-sauce-bottles',
-      solution3DID:  '652',
-      containerID: 'green-sauce',
-      onLoadingChanged: function(loading) {
-        loadingBar.style.width = loading.progress + "%";
-      }
-    };
-    Unlimited3D.init(options, {},function(error, status){
-        loadingContent.style.display = "none";
-        if (error || !status) {
-            console.log(error);
-            return;
-        }
-    });
+$(window).on("load", function() {
+  var loadingBar = document.getElementById("loadingBar");
+  var loadingContent = document.getElementById("loadingContent");
+  var options = {
+    distID: "1507",
+    solution3DName: "green-true-mild-sauce",
+    projectName: "hot-sauce-bottles",
+    solution3DID: "652",
+    containerID: "green-sauce",
+    onLoadingChanged: function(loading) {
+      loadingBar.style.width = loading.progress + "%";
+    }
+  };
+  Unlimited3D.init(options, {}, function(error, status) {
+    loadingContent.style.display = "none";
+    if (error || !status) {
+      console.log(error);
+      return;
+    }
+  });
 });
